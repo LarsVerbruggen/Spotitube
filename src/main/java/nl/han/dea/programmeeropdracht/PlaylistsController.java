4dcbcc7ad0ae257c104335d8b1a4ef866ad63ffb
Lars Verbruggen <larsverbruggen@hotmail.com>
2019-03-12 12:17:10 +0100

    PlaylistController track inladen per playlist. 500 Error op Playlist playlist declaratie
    -- PLAYLIST WORKING --
    
    TODO: Tracks inladen en laten zien op de website

--- src/main/java/nl/han/dea/programmeeropdracht/PlaylistsController.java
+++ src/main/java/nl/han/dea/programmeeropdracht/PlaylistsController.java
@@ -10,21 +10,24 @@ import java.util.ArrayList;
 @Path("playlists")
 public class PlaylistsController {
 
+
+    PlaylistsResponse response = new PlaylistsResponse();
+
     @GET
     @Produces("application/json")
     @Consumes("application/json")
     public Response getPlaylists(@QueryParam("token") String token){
 
-        PlaylistsResponse response = new PlaylistsResponse();
-
         Playlist deathMetal = new Playlist();
+        Playlist pop = new Playlist();
+
         deathMetal.setId(1);
         deathMetal.setName("Death Metal");
         deathMetal.setOwner(true);
         deathMetal.setTracks(new ArrayList<>());
 
 
-        Playlist pop = new Playlist();
+
         pop.setId(2);
         pop.setName("Pop");
         pop.setOwner(false);
@@ -39,16 +42,41 @@ public class PlaylistsController {
     }
 
 
-    @Path("{id}/tracks")
+    @Path("/{id}/tracks")
     @GET
     @Produces("application/json")
     @Consumes("application/json")
     public Response getTracksOfPlaylist(@QueryParam("token") String token, @PathParam("id") int id){
 
+        Playlist playlist = response.getPlaylists().get(id);
+
+        ArrayList<Track> tracks = new ArrayList<>();
+        Track track1 = new Track();
+        track1.setId(1);
+        track1.setTitle("Song for Someone");
+        track1.setPerformer("The Frames");
+        track1.setDuration(350);
+        track1.setAlbum("The cost");
+        track1.setOfflineAvailable(false);
+
+        Track track2 = new Track();
+        track2.setId(2);
+        track2.setTitle("The cost");
+        track2.setPerformer("The Frames");
+        track2.setDuration(423);
+        track2.setPlaycount(37);
+        track2.setPublicationDate("10-01-2005");
+        track2.setDescription("Title song from the Album The Cost");
+        track2.setOfflineAvailable(true);
+
+        tracks.add(track1);
+        tracks.add(track2);
+
+        playlist.setTracks(tracks);
 
 
-        // return Response.ok().entity(response).build();
-        return Response.status(502).build();
+        return Response.ok().entity(response).build();
+       // return Response.status(502).build();
     }
 
 }
